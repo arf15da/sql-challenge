770a165e2ebb4ad2125edaffff3143d8f0d4b2fa
arf15da <lilboila27@gmail.com>
2023-06-05 21:14:35 -0500

    Uploading final SQL codes, worked on this today.

---
+++ EmployeeSQL/Schema.sql
@@ -0,0 +1,65 @@
+--Create Tables for Import
+--Drop Tables if Existing
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+Create Table departments (
+	dept_no varchar primary key,
+	dept_name varchar
+);
+
+Create Table dept_emp (
+	emp_no int,
+	dept_no varchar
+);
+
+Create Table dept_manager (
+	dept_no varchar,
+	emp_no int
+);
+
+Create Table employees (
+	emp_no int primary key,
+	emp_title_id varchar,
+	birth_date date,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date date
+);
+
+Create Table salaries (
+	emp_no int,
+	salary int
+);
+
+Create Table titles (
+	title_id varchar primary key,
+	title varchar
+);
+
+--Import CSVs in Order
+
+--Then Add Foreign Keys to relate the tables to one another
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
